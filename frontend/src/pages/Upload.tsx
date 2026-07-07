f72d9e65b8bacf01b1c2e4d2235ae411c9ee1a90
rutwikrp <rutwikpatil73@gmail.com>
2026-07-07 13:56:37 +0000

    added upload page

---
+++ frontend/src/pages/Upload.tsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import UploadCard from "@/components/upload/UploadCard";
+import DocumentTable from "@/components/upload/DocumentTable";
+import { uploadDocument } from "@/services/documentService";
+
+export default function Upload() {
+
+  const [selectedFile, setSelectedFile] =
+    useState<File | null>(null);
+
+  const [uploading, setUploading] =
+    useState(false);
+
+  async function handleUpload() {
+
+    if (!selectedFile) {
+      alert("Select a PDF first");
+      return;
+    }
+
+    try {
+
+      setUploading(true);
+
+      const response =
+        await uploadDocument(selectedFile);
+
+      console.log(response);
+
+      alert("Document uploaded successfully!");
+
+      setSelectedFile(null);
+
+    } catch (err) {
+
+      console.error(err);
+
+      alert("Upload failed.");
+
+    } finally {
+
+      setUploading(false);
+
+    }
+
+  }
+
+  return (
+    <>
+      <div className="mb-8">
+        <h1 className="text-3xl font-bold">
+          Documents
+        </h1>
+
+        <p className="text-gray-500">
+          Upload and manage your knowledge base
+        </p>
+      </div>
+
+      <UploadCard
+          selectedFile={selectedFile}
+          uploading={uploading}
+          onFileChange={(e) =>
+        setSelectedFile(e.target.files?.[0] || null)
+    }
+          onUpload={handleUpload}
+      />
+
+      <DocumentTable />
+    </>
+  );
+}
