6d34013bc0e6a8bab9bf2f181b88e3e968e7a1ff
rutwikrp <rutwikpatil73@gmail.com>
2026-07-12 15:19:22 +0000

    fixed api path

--- backend/main.py
+++ backend/main.py
@@ -12,9 +12,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
+        "*",
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
